040b4c9f65d38c88c2f3b84ce6a69e2f2d790f08
MATHEUS HENRY DOS SANTOS <25204051@dsn127m1241958.cfp127aluno.local>
2026-07-27 16:55:18 -0300

    Criação da área de listar os alunos de uma turma quase concluida, faltado apenas o menu de status

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 import {getAlunos, getAlunosNoCurso, getCursos, getDetalhesAluno, getStatusAlunos} from "./rotas.js"
 
 const container = document.getElementById("container")
+const header = document.getElementById("header")
 
 async function exibirCursos(){
     try {
@@ -40,11 +41,117 @@ async function exibirCursos(){
 }
 
 async function exibirTurma(curso){
-    if(curso == "Desenvolvimento de Sistemas"){
 
-    }else{
+    container.innerHTML = ""
+    container.style.flexDirection = "column"
+    header.style.height = "120px"
+
+    const barraStatus = document.createElement("div")
+    barraStatus.className = "barra-status"
+
+    const buttonStatus = document.createElement("button")
+    buttonStatus.className = "button-status"
+    buttonStatus.textContent = "Status"
+    buttonStatus.addEventListener("click", async () => {
+        const menuStatus = document.createElement("div")
+        menuStatus.className = "menu-status"
+
         
+
+        container.appendChild(menuStatus)
+    })
+
+    const legenda = document.createElement("div")
+    legenda.className = "legenda"
+
+    const legendaTitulo = document.createElement("span")
+    legendaTitulo.classList.add("legenda-text")
+    legendaTitulo.textContent = "LEGENDA"
+
+    const quadradoAzul = document.createElement("div")
+    quadradoAzul.classList.add("quadrado-azul")
+
+    const cursando = document.createElement("span")
+    cursando.classList.add("legenda-text")
+    cursando.textContent = "Cursando"
+
+    const quadradoAmarelo = document.createElement("div")
+    quadradoAmarelo.classList.add("quadrado-amarelo")
+
+    const finalizado = document.createElement("span")
+    finalizado.classList.add("legenda-text")
+    finalizado.textContent = "Finalizado"
+
+    const tituloCurso = document.createElement("h1")
+    tituloCurso.className = "titulo-curso"
+    tituloCurso.textContent = curso
+
+    const cardsAlunos = document.createElement("div")
+    cardsAlunos.className = "cards-alunos"
+
+    if(curso == "Desenvolvimento de Sistemas"){
+        const alunos = await getAlunosNoCurso(1)
+
+        for(let aluno of alunos){
+            const cardAluno = document.createElement("div")
+            cardAluno.className = "card-aluno"
+            cardAluno.addEventListener("click", () => {
+                exibirDadosDoAluno(aluno)
+            })
+
+            const imgAluno = document.createElement("img")
+            imgAluno.src = aluno.foto
+            imgAluno.alt = "Imagem de um usuário"
+
+            const nomeAluno = document.createElement("h3")
+            nomeAluno.textContent = aluno.nome
+
+            cardAluno.append(imgAluno, nomeAluno)
+            cardsAlunos.append(cardAluno)
+
+            if(aluno.status == "cursando"){
+                cardAluno.classList.add("cursando")
+            }else if(aluno.status == "finalizado"){
+                cardAluno.classList.add("finalizado")
+            }
+        }
+    }else{
+        const alunos = await getAlunosNoCurso(2)
+
+        for(let aluno of alunos){
+            const cardAluno = document.createElement("div")
+            cardAluno.className = "card-aluno"
+            cardAluno.style.cursor = "pointer"
+            cardAluno.addEventListener("click", () => {
+                exibirDadosDoAluno(aluno)
+            })
+
+            const imgAluno = document.createElement("img")
+            imgAluno.src = aluno.foto
+            imgAluno.alt = "Imagem de um usuário"
+
+            const nomeAluno = document.createElement("h3")
+            nomeAluno.textContent = aluno.nome
+
+            cardAluno.append(imgAluno, nomeAluno)
+            cardsAlunos.append(cardAluno)
+
+            if(aluno.status == "cursando"){
+                cardAluno.classList.add("cursando")
+            }else if(aluno.status == "finalizado"){
+                cardAluno.classList.add("finalizado")
+            }
+        }
     }
+    
+    legenda.append(legendaTitulo, quadradoAzul, cursando, quadradoAmarelo, finalizado)
+    barraStatus.append(buttonStatus, legenda)
+    header.append(barraStatus)
+    container.append(tituloCurso, cardsAlunos)
+}
+
+function exibirDadosDoAluno(aluno){
+
 }
 
 exibirCursos()
